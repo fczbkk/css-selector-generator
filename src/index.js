37ebb3f1bc4a2b9535a5e776c064f65863595deb
Riki Fridrich <riki@fczbkk.com>
2021-03-20 14:44:47 +0100

    chore: fix formating of long jsdoc lines

--- src/index.js
+++ src/index.js
@@ -44,26 +44,15 @@ export default getCssSelector;
 
 /**
  * @typedef {Object} css_selector_generator_options
- * @property {Array.<css_selector_type>} [selectors] - List of selector types
- *   to use. They will be prioritised by their order.
- * @property {Array.<RegExp | string>} [whitelist] - List of selectors that
- *   should be prioritised.
- * @property {Array.<RegExp | string>} [blacklist] - List of selectors that
- *   should be ignored.
- * @property {Element} [root] - Root element inside which the selector will be
- *   generated. If not set, the document root will be used.
- * @property {boolean} [combineWithinSelector = true] - If set to `true`, the
- *   generator will test combinations of selectors of single type (e.g.
- *   multiple class selectors).
- * @property {boolean} [combineBetweenSelectors = true] - If set to `true`, the
- *   generator will try to test combinations of selectors of different types
- *   (e.g. tag + class name).
- * @property {boolean} [includeTag = false] - If set to `true`, all generated
- *   selectors will include the TAG part. Even if tag selector type is not
- *   included in `selectors` option.
+ * @property {Array.<css_selector_type>} [selectors] - List of selector types to use. They will be prioritised by their order.
+ * @property {Array.<RegExp | string>} [whitelist] - List of selectors that should be prioritised.
+ * @property {Array.<RegExp | string>} [blacklist] - List of selectors that should be ignored.
+ * @property {Element} [root] - Root element inside which the selector will be generated. If not set, the document root will be used.
+ * @property {boolean} [combineWithinSelector = true] - If set to `true`, the generator will test combinations of selectors of single type (e.g. multiple class selectors).
+ * @property {boolean} [combineBetweenSelectors = true] - If set to `true`, the generator will try to test combinations of selectors of different types (e.g. tag + class name).
+ * @property {boolean} [includeTag = false] - If set to `true`, all generated selectors will include the TAG part. Even if tag selector type is not included in `selectors` option.
  */
 
 /**
- * @typedef {'id' | 'class' | 'tag' | 'attribute' | 'nthchild' | 'nthoftype'}
- *   css_selector_type
+ * @typedef {'id' | 'class' | 'tag' | 'attribute' | 'nthchild' | 'nthoftype'} css_selector_type
  */
