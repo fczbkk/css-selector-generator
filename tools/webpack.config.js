3712401351caf8bb4efae5a0fab1e2410b035cb0
Federico Brigante <me@fregante.com>
2021-06-27 23:36:49 +0700

    Drop babel-loader

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -11,16 +11,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
         test: /\.html$/,
         use: {
           loader: 'raw-loader'
