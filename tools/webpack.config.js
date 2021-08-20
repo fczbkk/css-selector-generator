17e1e2bc1c376c915a1e82b45c8e95ac2fd0055a
fczbkk <riki@fczbkk.com>
2021-08-20 08:13:57 +0200

    chore: remove Babel dependency

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -16,16 +16,6 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
         test: /\.html$/,
         use: {
           loader: 'raw-loader',
