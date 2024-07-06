3913204f85cc4c6f21567ee6bc516217caf3f476
fczbkk <riki@fczbkk.com>
2024-07-06 17:49:06 +0200

    refactor: remove BlankPage Playwright fixture

--- playwright-tests/fixtures.ts
+++ playwright-tests/fixtures.ts
@@ -1,17 +1,11 @@
 import { test as base } from "@playwright/test";
-import { BlankPage } from "./fixture.blank-page.js";
 import { LibraryPage } from "./fixture.library-page.js";
 
 interface Fixtures {
-  blankPage: BlankPage;
   libraryPage: LibraryPage;
 }
 
 export const test = base.extend<Fixtures>({
-  blankPage: async ({ page }, use) => {
-    const blankPage = new BlankPage(page);
-    await use(blankPage);
-  },
   libraryPage: async ({ page }, use) => {
     const libraryPage = new LibraryPage(page);
     await use(libraryPage);
